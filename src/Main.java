47a2cb620f700561044445654cd80baa201d4d86
Mirlan Tlegenov <tlegenovmirlan244@gmail.com>
2026-02-04 11:46:36 +0500

    refactor main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 import controllers.ConsoleController;
 
 public class Main {
+
     public static void main(String[] args) {
+        startApplication();
+    }
+
+    private static void startApplication() {
         ConsoleController controller = new ConsoleController();
         controller.start();
     }
