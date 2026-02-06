d43b0af5a697afc0b9424fe4b1654a05529b651e
Mirlan Tlegenov <tlegenovmirlan244@gmail.com>
2026-02-06 23:30:55 +0500

    Update CashierDialog messages

--- src/ui/CashierDialog.java
+++ src/ui/CashierDialog.java
@@ -4,23 +4,23 @@ public class CashierDialog {
 
     public void hello() {
         System.out.println("\nCashier: Hello! Welcome to our ticket office.");
-        System.out.println("Cashier: You can enter 0 at any time to cancel.");
+        System.out.println("Cashier: Type 0 anytime to cancel.");
     }
 
     public void askFlight() {
-        System.out.println("Cashier: Please choose a flight.");
+        System.out.println("Cashier: Choose a flight.");
     }
 
     public void confirm() {
-        System.out.println("Cashier: Booking your ticket now...");
+        System.out.println("Cashier: Processing your booking...");
     }
 
     public void done(int bookingId) {
-        System.out.println("Cashier: Your booking ID is " + bookingId + ".");
+        System.out.println("Cashier: Your booking ID is " + bookingId);
         System.out.println("Cashier: Have a nice flight!");
     }
 
     public void cancelledByUser() {
-        System.out.println("Cashier: No problem. Returning to menu.");
+        System.out.println("Cashier: Returning to menu.");
     }
 }
