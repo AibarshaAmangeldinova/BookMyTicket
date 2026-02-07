2d6561163172a2f41bc538eb96b9139589e1bcb1
Mirlan Tlegenov <tlegenovmirlan244@gmail.com>
2026-02-07 08:52:11 +0500

    feat: add DocumentType enum

--- src/models/DocumentType.java
+++ src/models/DocumentType.java
@@ -1,6 +1,12 @@
 package models;
 
 public enum DocumentType {
-    PASSPORT, ID_CARD
+    PASSPORT,
+    ID_CARD;
+
+    public static DocumentType fromString(String value) {
+        return DocumentType.valueOf(value.toUpperCase());
+    }
 }
+
 
